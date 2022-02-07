3f0ad551903f474e1d8efae798ff0ca0c3e78bd9
ingong <insong1216@gmail.com>
2022-02-07 22:18:23 +0900

    feat: 백준 INPUT 코드 추가

---
+++ INPUT/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+// BAEKJOON
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// VSCODE : TEST_CASE 폴더 생성 후, 원하는 테스트 케이스를 index.txt 에 작성
+const input = fs.readFileSync('TEST_CASE/index.txt').toString().trim().split('\n');
+const log = console.log;
