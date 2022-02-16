e5c65e9dc5f98a5bae8a4e6aabec898eb42bd3c6
ingong <insong1216@gmail.com>
2022-02-16 17:36:25 +0900

    BackTracking: N과과M (1)~(4)

---
+++ 13_Backtracking/15650_N과M(2).js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+// BAEKJOON
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// VSCODE : TEST_CASE 폴더 생성 후, 원하는 테스트 케이스를 index.txt 에 작성
+const input = fs.readFileSync('TEST_CASE/index.txt').toString().trim().split('\n');
+const log = console.log;
+const [N, M] = input[0].split(' ').map(Number);
+
+const visited = new Array(N).fill(false);
+const output = [];
+let result = '';
+
+function dfs(cnt) {
+  if (cnt === M) {
+    result += `${output.join(' ')}\n`;
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (visited[i]) continue;
+    if (output[output.length - 1] > i + 1) continue;
+    // ouput 의 마지막 값보다 작거나 같다면 continue
+    visited[i] = true;
+    output.push(i + 1);
+    dfs(cnt + 1);
+    output.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0);
+// 양쪽 공백을 제거하기 위해 사용하는 함수
+log(result.trim());
