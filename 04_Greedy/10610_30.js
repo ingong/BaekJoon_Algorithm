da5e503d931ad5f3dd9c5ad060ac8e8216df9b64
ingong <insong1216@gmail.com>
2022-02-27 15:44:40 +0900

    Greedy: 10610: 30.js

--- 04_Greedy/10610_30.js
+++ 04_Greedy/10610_30.js
@@ -4,6 +4,15 @@ const fs = require('fs');
 // VSCODE : TEST_CASE 폴더 생성 후, 원하는 테스트 케이스를 index.txt 에 작성
 const input = fs.readFileSync('TEST_CASE/index.txt').toString().trim().split('\n');
 const log = console.log;
-const solution = (input) => { };
+const solution = (input) => {
+  const numbers = input[0].split('').map(Number);
+  const sum = numbers.reduce((a, b) => a + b, 0);
+  if (!numbers.includes(0) || (sum % 3 !== 0)) {
+    log(-1);
+    return;
+  } else {
+    log(numbers.sort((a, b) => b - a).join(''));
+  }
+};
 
 solution(input);
