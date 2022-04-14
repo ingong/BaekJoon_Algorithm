54796ad0d0ae98faf198ce88c40a1caeb7a284ea
ingong <insong1216@gmail.com>
2022-04-14 16:02:08 +0900

    [FEAT]: InsertionSort

---
+++ 16. Sort/InsertionSort.js
@@ -0,0 +1,15 @@
+const numbers = [99, 44, 6, 1, 5, 283, 4, 0];
+const insertionSort = (array) => {
+  const length = array.length;
+  for (let i = 0; i < length; i++){
+    if (array[i] < array[0]) array.unshift(array.splice(i, 1)[0]);
+    else {
+      for (let j = 1; j < i; j++){
+        if (array[j - 1] < array[i] && array[i] < array[j]) array.splice(j, 0, array.splice(i, 1)[0]);
+      }
+    }
+  }
+}
+
+insertionSort(numbers);
+console.log(numbers); // [0, 1, 4, 5, 6, 44, 99, 283]
--- 16. Sort/SelectionSort.js
+++ 16. Sort/SelectionSort.js
@@ -1,6 +1,4 @@
-const log = console.log;
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
-
+const numbers = [99, 44, 6, 1, 5, 283, 4, 0];
 const selectionSort = (array) => {
   const length = array.length;
   for (let i = 0; i < length; i++){
@@ -13,4 +11,4 @@ const selectionSort = (array) => {
 }
 
 selectionSort(numbers);
-log(numbers);
+console.log(numbers); // [0, 1, 4, 5, 6, 44, 99, 283]
