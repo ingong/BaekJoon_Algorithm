314ee25d1ef55c7192ba72fce76d2ae48b8ae1af
ingong <insong1216@gmail.com>
2022-04-23 20:51:00 +0900

    [FEAT]: linkedList, double linkedList 구현

--- 19. DataStructre/linkedList.js
+++ 19. DataStructre/linkedList.js
@@ -43,9 +43,9 @@ class LinkedList {
       
     const newNode = new Node(value);
     const leader = this.traverseToIndex(index - 1);
-    const holdingPointer = leader.next;
+    const follower = leader.next;
     leader.next = newNode;
-    newNode.next = holdingPointer;
+    newNode.next = follower;
     this.length++;
   }
   remove(index) {
@@ -73,13 +73,27 @@ class LinkedList {
     }
     return currentNode;
   }
+  reverse() {
+    if (!this.head.next) return this.head;
+
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null; // error
+    this.head = first;
+    return this.printList();
+  }
 }
 
 let myLinkedList = new LinkedList(10);
-myLinkedList.append(5);
+myLinkedList.append(12);
 myLinkedList.append(16);
-myLinkedList.prepend(1);
-myLinkedList.insert(1, 2);
-myLinkedList.insert(1, 4);
-myLinkedList.remove(2);
+myLinkedList.append(20);
+myLinkedList.reverse();
 console.log(myLinkedList.printList())
